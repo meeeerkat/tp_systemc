c74201f0ead03229019ce4d8ceceb54544371740
Paul Suau <paul.suau@grenoble-inp.org>
2023-01-04 11:19:36 +0000

    tp1q4

--- TPs/tp1/generator.cpp
+++ TPs/tp1/generator.cpp
@@ -4,13 +4,9 @@
 using namespace std;
 
 void Generator::thread(void) {
-	ensitlm::data_t val = 10;
 	ensitlm::addr_t addr = 0x10000000;
-  for (ensitlm::addr_t i=0; i < 10; i++)
-		socket.write(addr+4*i, val);
-
-	socket.write(addr+2, val);
-	socket.write(3*addr, val);
+  for (ensitlm::data_t i=0; i < 0x100; i++)
+		socket.write(addr+4*i, i);
 }
 
 Generator::Generator(sc_core::sc_module_name name) : sc_core::sc_module(name) {
--- TPs/tp1/sc_main.cpp
+++ TPs/tp1/sc_main.cpp
@@ -32,5 +32,14 @@ int sc_main(int argc, char **argv) {
 
 	/* and start simulation */
 	sc_core::sc_start();
+
+
+  ensitlm::data_t d=10;
+  for (ensitlm::addr_t a=0; a < 0x100; a+=4) {
+    if (mem.read(a, d) != tlm::TLM_OK_RESPONSE)
+      std::cout << "ERROR at " << a << std::endl;
+  }
+
+
 	return 0;
 }
